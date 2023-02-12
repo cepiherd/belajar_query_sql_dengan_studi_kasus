949cfd053f79e47c3996efad13a292600759fa10
Ricki Chandra Hidayatullah <53082147+rickichann@users.noreply.github.com>
2023-02-12 19:24:49 +0700

    Add files via upload

---
+++ playlist_3_menampilkan_detail_pesanan.sql
@@ -0,0 +1,27 @@
+
+-- versi tutorial
+SELECT  
+    a.OrderDate,
+    a.OrderID,
+    b.CustomerName,
+    c.ProductID,
+    c.Quantity
+FROM Orders as a
+JOIN Customers as b ON a.CustomerID = b.CustomerID
+JOIN OrderDetails as c ON a.OrderID = c.OrderID
+WHERE a.CustomerID = 61
+
+
+-- versi lengkap (ditambahkan relasti dengan table products)
+SELECT  
+    a.OrderDate,
+    a.OrderID,
+    b.CustomerName,
+    c.ProductID,
+    c.Quantity,
+    d.ProductName
+FROM Orders as a
+JOIN Customers as b ON a.CustomerID = b.CustomerID
+JOIN OrderDetails as c ON a.OrderID = c.OrderID
+JOIN Products as d ON c.ProductID = d.ProductID
+WHERE a.CustomerID = 61
